4381ba7a4c6ebc575da5c5e8aa75602576661bb5
tejas-tank <tejasget2learn@gmail.com>
2022-04-01 00:14:37 +0530

    add

---
+++ JS/Googlejspage.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  document.getElementById("searchField").focus();
+};
+
+function setShadow() {
+  document
+    .getElementsByClassName("searchBar")[0]
+    .setAttribute("class", "searchBar shadow");
+}
+function removeShadow() {
+  document
+    .getElementsByClassName("searchBar")[0]
+    .setAttribute("class", "searchBar");
+}
+
+function setFocusAndShadow() {
+  setFocus();
+  setShadow();
+}
+
+document.getElementById("searchField").addEventListener("focusin", function () {
+  setShadow();
+});
+document
+  .getElementById("searchField")
+  .addEventListener("focusout", function () {
+    removeShadow();
+  });
